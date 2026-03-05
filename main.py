585a5147c8fc66cb8bfb49bb5b9262a1f3fd06d2
sgpkey35 <116185381+sgpkey35@users.noreply.github.com>
2026-03-05 14:35:07 +0300

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T11:34:43.424Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T11:35:06.329Z
+Commit: Add multi-language support
